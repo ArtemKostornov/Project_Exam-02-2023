cf5f9c950485a4c894a3e1be0d22d6ec3a9e1821
Artem Kostornov <ArtKosto@yandex.ru>
2023-02-26 18:55:41 +0300

    add function

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿
 string[] array = { "1", "two", "3", "four", "five" };
 string[] arr = new string[5];
+
+
+string[] DoArr(string[] array, string[] arr)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            arr[i] = array[i];
+        }
+    }
+    return arr;
+}
